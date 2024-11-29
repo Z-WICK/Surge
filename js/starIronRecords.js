104617d18433d3d51d71de65f60383f066ce898f
Z-WICK <2289431216@qq.com>
2024-11-30 02:33:07 +0800

    fix: 优化message

--- js/starIronRecords.js
+++ js/starIronRecords.js
@@ -1,5 +1,5 @@
 /**
- * @fileoverview 抓取 Gacha 参数并推送到 Bark
+ * @fileoverview 抓取 Gacha 请求的 URL 并推送到 Bark
  */
 
 // 从环境变量中获取 BarkUrl（兼容手动传参）
@@ -27,28 +27,10 @@ try {
     const url = $request.url;
     log(`完整的请求 URL: ${url}`);
 
-    // 提取 URL 参数
-    const queryParams = new URLSearchParams(url.split('?')[1]);
-    const params = {};
-    queryParams.forEach((value, key) => {
-        params[key] = value;
-    });
-
-    // 打印解析到的参数内容
-    if (Object.keys(params).length === 0) {
-        log("未抓取到任何参数，可能是接口问题。", "WARN");
-        throw new Error("未抓取到参数");
-    } else {
-        log(`抓取的参数内容:\n${JSON.stringify(params, null, 2)}`);
-    }
-
-    // 格式化参数为 JSON 字符串
-    const message = `Gacha Log Parameters:\n${JSON.stringify(params, null, 2)}`;
-
     // Bark 推送内容
     const barkPayload = {
         title: "星铁抽卡记录",
-        body: message,
+        body: `请求 URL: ${url}`,
         group: "星铁记录", // 可选分组，方便管理
     };
 
@@ -57,7 +39,7 @@ try {
     $httpClient.post(
         {
             url: barkUrl,
-            body: url,
+            body: JSON.stringify(barkPayload),
             headers: { "Content-Type": "application/json" },
         },
         (error, response, data) => {
