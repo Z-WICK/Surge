a8bbd9d78ab8e4fffcf34425ad6aa52fd416e1f3
Z-WICK <2289431216@qq.com>
2025-05-17 21:12:14 +0800

    feat: jd_cookie debug模式

--- js/jd_cookie_tg.js
+++ js/jd_cookie_tg.js
@@ -7,10 +7,11 @@
 (async () => {
     const chatId = typeof $argument !== "undefined" ? $argument.match(/chatId=([^&]+)/)?.[1] : null;
     const botToken = typeof $argument !== "undefined" ? $argument.match(/botToken=([^&]+)/)?.[1] : null;
-    let telegramApiUrl = 'https://api.telegram.org/bot'+botToken+'/sendMessage'; // 替换为你的 Telegram Bot API Token
+    let telegramApiUrl = 'https://api.telegram.org/bot' + botToken + '/sendMessage'; // 替换为你的 Telegram Bot API Token
 
     // 调试模式开关
-    const DEBUG_MODE = false; // 如果为 true，将跳过去重检查，直接发送推送
+    const DEBUG_MODE = typeof $argument !== "undefined" && $argument.includes("Debug=true"); // 如果为 true，将跳过去重检查，直接发送推送
+
 
     // 本地存储，用于记录已抓取的数据
     let savedData = $persistentStore.read("JD_Cookie_Data") || "{}";
